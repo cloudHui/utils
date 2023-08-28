c3abc11035a6156be9b9f23aee585c7e88ea0749
liuyunhui <liuyunhui@coolfishgames.com>
2023-08-28 07:15:30 +0000

    update

--- src/main/java/utils/utils/ExecCommand.java
+++ src/main/java/utils/utils/ExecCommand.java
@@ -4,19 +4,18 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
 
 public class ExecCommand {
 
 	/**
 	 * 执行命令
 	 *
+	 * @param command        命令
+	 * @param needResultChar 需要的结果字符串
 	 * @return 命令结果
 	 */
-	public static String exeCommand(String command) {
+	public static String exeCommand(String command, String needResultChar) {
 		String cmd;
-		List<String> results = new ArrayList<>();
 		String osName = System.getProperty("os.name");
 		if (osName.contains("Windows")) {
 			cmd = "cmd.exe /c " + command;
@@ -24,23 +23,22 @@ public class ExecCommand {
 			cmd = command;
 		}
 		InputStream in;
-		String result;
+		String result = "";
 		try {
 			Process pro = Runtime.getRuntime().exec(cmd);
 			in = pro.getInputStream();
 			BufferedReader read = new BufferedReader(new InputStreamReader(in));
 			if ((result = read.readLine()) != null) {
 				do {
-					results.add(result);
+					if (needResultChar != null && needResultChar.length() > 0 && result.contains(needResultChar)) {
+						return result;
+					}
 				} while (read.readLine() != null);
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		if (!results.isEmpty()) {
-			return results.get(0);
-		}
-		return "";
+		return result;
 	}
 
 	/**
@@ -62,6 +60,6 @@ public class ExecCommand {
 	}
 
 	public static void main(String[] args) {
-		System.out.println(exeCommand("svn info"));
+		System.out.println(exeCommand("svn info", ""));
 	}
 }
